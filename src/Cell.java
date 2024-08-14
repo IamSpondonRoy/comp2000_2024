45473621222fc31f0d9a714fad8d887ebdd4e50d
IamSpondonRoy <spondonroy.rohan@students.mq.edu.au>
2024-08-14 11:31:12 +1000

    Milestone_part1

--- src/Cell.java
+++ src/Cell.java
@@ -3,34 +3,32 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 public class Cell {
-  // fields
-  int x;
-  int y;
-  static int size = 35;
+    int x;
+    int y;
+    static int size = 35;
 
-  // constructors
-  public Cell(int inX, int inY) {
-    x = inX;
-    y = inY;
-  }
-
-  // methods
-  public void paint(Graphics g, Point mousePos) {
-    if(contains(mousePos)) {
-      g.setColor(Color.GRAY);
-    } else {
-      g.setColor(Color.WHITE);
+    public Cell(int inX, int inY) {
+        x = inX;
+        y = inY;
     }
-    g.fillRect(x, y, size, size);
-    g.setColor(Color.BLACK);
-    g.drawRect(x, y, size, size);
-  }
 
-  public boolean contains(Point p) {
-    if(p != null) {
-      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
-    } else {
-      return false;
+
+    public void paint(Graphics g, Point mousePos) {
+        if (contains(mousePos)) {
+            g.setColor(Color.gray);
+        } else {
+            g.setColor(Color.white);
+        }
+        g.fillRect(x, y, size, size);
+        g.setColor(Color.green);
+        g.drawRect(x, y, size, size);
     }
-  }
+
+    public boolean contains(Point p) {
+        if (p != null) {
+            return x < p.x && x + size > p.x && y < p.y && y + size > p.y;
+        } else {
+            return false;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,62 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
-
+import java.awt.Point;
+import java.awt.Color;
+import java.util.LinkedList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-      Main window = new Main();
-      window.run();
+        Main window = new Main();
+        window.run();
     }
 
     class Canvas extends JPanel {
-      Grid grid = new Grid();
-      public Canvas() {
-        setPreferredSize(new Dimension(720, 720));
-      }
+        Grid grid = new Grid();
+        LinkedList<Point> trail_mouse = new LinkedList<>();
+        int trail_size = 100;
+        int circle_size = 15;
 
-      @Override
-      public void paint(Graphics g) {
-        grid.paint(g, getMousePosition());
-      }
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            Point mousePos = getMousePosition();
+            if (mousePos != null) {
+                if (trail_mouse.size() >= trail_size) {
+                  trail_mouse.removeFirst();
+                }
+                trail_mouse.addLast(mousePos);
+            }
+            grid.paint(g, mousePos);
+            drawMouseTrail(g);
+        }
+
+        private void drawMouseTrail(Graphics g) {
+            for (int item = 0; item < trail_mouse.size(); item+=1) {
+                Point p = trail_mouse.get(item);
+                int transperancy = (int) (255 * ((double) (item + 1) / trail_size)); // why double feels better but not float
+                g.setColor(new Color(10, 70, 10, transperancy));
+                g.fillOval(p.x - circle_size / 2, p.y - circle_size / 2, circle_size, circle_size);
+            }
+        }
     }
 
     private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
     }
 
     public void run() {
-      while(true) {
-        repaint();
-      }
+        while (true) {
+            repaint();
+        }
     }
 }
+// Complete Part 1
