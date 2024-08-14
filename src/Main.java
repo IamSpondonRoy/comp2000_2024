063811b0af6f9d258877d12c4823398a08511cec
IamSpondonRoy <spondonroy.rohan@students.mq.edu.au>
2024-08-14 11:34:38 +1000

    Milestone1_Part2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.util.LinkedList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
@@ -12,7 +15,7 @@ public class Main extends JFrame {
         window.run();
     }
 
-    class Canvas extends JPanel {
+    class Canvas extends JPanel implements MouseMotionListener{
         Grid grid = new Grid();
         LinkedList<Point> trail_mouse = new LinkedList<>();
         int trail_size = 100;
@@ -20,18 +23,19 @@ public class Main extends JFrame {
 
         public Canvas() {
             setPreferredSize(new Dimension(720, 720));
+            addMouseMotionListener(this);
         }
 
         @Override
         public void paint(Graphics g) {
-            Point mousePos = getMousePosition();
-            if (mousePos != null) {
-                if (trail_mouse.size() >= trail_size) {
-                  trail_mouse.removeFirst();
-                }
-                trail_mouse.addLast(mousePos);
-            }
-            grid.paint(g, mousePos);
+            // Point mousePos = getMousePosition();
+            // if (mousePos != null) {
+            //     if (trail_mouse.size() >= trail_size) {
+            //       trail_mouse.removeFirst();
+            //     }
+            //     trail_mouse.addLast(mousePos);
+            // }
+            grid.paint(g, null);
             drawMouseTrail(g);
         }
 
@@ -43,8 +47,22 @@ public class Main extends JFrame {
                 g.fillOval(p.x - circle_size / 2, p.y - circle_size / 2, circle_size, circle_size);
             }
         }
+        
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            Point mousePos = e.getPoint();
+            if (trail_mouse.size() >= trail_size) {
+              trail_mouse.removeFirst();
+            }
+            trail_mouse.addLast(mousePos);
+            repaint();
+        }
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            mouseMoved(e);
+        }
     }
-
+    
     private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Canvas canvas = new Canvas();
@@ -55,8 +73,7 @@ public class Main extends JFrame {
 
     public void run() {
         while (true) {
-            repaint();
         }
     }
 }
-// Complete Part 1
+// Complete part2
