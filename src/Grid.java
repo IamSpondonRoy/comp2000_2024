57c18cf9deac9e62c16ac523401393bf67dfaf92
IamSpondonRoy <spondonroy.rohan@students.mq.edu.au>
2024-08-21 12:33:49 +1000

    Complete Task12

--- src/Grid.java
+++ src/Grid.java
@@ -1,38 +1,57 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.Optional;
 
 public class Grid {
-  Cell[][] cells = new Cell[20][20];
+    Cell[][] cells = new Cell[20][20];
   
-  public Grid() {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j] = new Cell(colToLabel(i), j, 10+Cell.size*i, 10+Cell.size*j);
-      }
+    public Grid() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(colToLabel(i), j, 10 + Cell.size * i, 10 + Cell.size * j);
+            }
+        }
     }
-  }
 
-  private char colToLabel(int col) {
-    return (char) (col + Character.valueOf('A'));
-  }
-
-  private int labelToCol(char col) {
-    return (int) (col - Character.valueOf('A'));
-  }
-
-  public void paint(Graphics g, Point mousePos) {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j].paint(g, mousePos);
-      }
+    private char colToLabel(int col) {
+        return (char) (col + Character.valueOf('A'));
     }
-  }
 
-  public Cell cellAtColRow(int c, int r) {
-    return cells[c][r];
-  }
+    private int labelToCol(char col) {
+        return (int) (col - Character.valueOf('A'));
+    }
 
-  public Cell cellAtColRow(char c, int r) {
-    return cellAtColRow(labelToCol(c), r);
-  }
+    public void paint(Graphics g, Point mousePos) {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j].paint(g, mousePos);
+            }
+        }
+    }
+
+    public Optional<Cell> cellAtColRow(int c, int r) {
+        if (c < 0 || c >= cells.length || r < 0 || r >= cells[c].length) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(cells[c][r]);
+    }
+
+    public Optional<Cell> cellAtColRow(char c, int r) {
+        return cellAtColRow(labelToCol(c), r);
+    }
+
+    public Optional<Cell> cellAtPoint(Point p) {
+        if (p == null) {
+            return Optional.empty();
+        }
+
+        int col = (p.x - 10) / Cell.size;
+        int row = (p.y - 10) / Cell.size;
+
+        if (col < 0 || col >= cells.length || row < 0 || row >= cells[col].length) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(cells[col][row]);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,52 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.io.IOException;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-      Main window = new Main();
-      window.run();
+        Main window = new Main();
+        window.run();
     }
 
     class Canvas extends JPanel {
-      Stage stage;
-      public Canvas() {
-        setPreferredSize(new Dimension(720, 720));
-        stage = StageReader.readStage("data/stage11.rvb");
-      }
+        Stage stage;
+        Point mousePos;
 
-      @Override
-      public void paint(Graphics g) {
-        stage.paint(g, getMousePosition());
-      }
+        public Canvas() {
+            setPreferredSize(new Dimension(1024, 720)); // Increase window size
+            try {
+                stage = StageReader.readStage("data/stage1.rvb");
+            } catch (IOException e) {
+                System.err.println("IOException occurred in Canvas constructor");
+                System.exit(1);
+            }
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            mousePos = getMousePosition();
+            stage.paint(g, mousePos);
+            stage.grid.cellAtPoint(mousePos).ifPresent(cell -> {
+                g.setColor(java.awt.Color.BLACK);
+                g.drawString("Cell: " + cell.col + cell.row, 740, 20);
+            });
+        }
     }
 
     private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
     }
 
     public void run() {
-      while(true) {
-        repaint();
-      }
+        while (true) {
+            repaint();
+        }
     }
 }
